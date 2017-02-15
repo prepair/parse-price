1c74862417619a934cd47d1480d473448f1af3b6
Kurdi Szabolcs <x-Szabolcs.Kurdi@wizzair.com>
2017-02-15 14:28:14 +0100

    test(sinon): sinon chai connector

--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,9 @@
 const sinon = require('sinon');
 const mocha = require('mocha');
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+chai.use(sinonChai);
 
 beforeEach(function () {
   this.sandbox = sinon.sandbox.create();
